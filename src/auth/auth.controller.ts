bf6e9e69c8148bb9829598ee7b287a810c63c0bc
foysalious <foysalrahman112@gmail.com>
2022-06-29 12:49:44 +0600

    authorization added

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -11,6 +11,7 @@ import {
   Req,
   ValidationPipe,
   UsePipes,
+  UnauthorizedException,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
@@ -23,6 +24,9 @@ export class AuthController {
 
   @Post('auth/register')
   async register(@Body() registerDto: RegisterDto, @Res() response: Response) {
+    if (registerDto.role == "Teacher")
+      throw new UnauthorizedException("You are not authorized")
+
     const user = await this.authService.register(registerDto);
     return response.status(201).send(user);
   }
@@ -38,6 +42,15 @@ export class AuthController {
   @UsePipes(new ValidationPipe({ transform: true }))
   async login(@Body() authCredentialDto: LoginDto) {
     return await this.authService.login(authCredentialDto);
+  }
+
+  @Post('auth/register/teacher')
+  async registerTeacher(@Body() registerDto: RegisterDto, @Res() response: Response) {
+    const userInfo = response.locals.userPayload
+    if (userInfo.role == "Teacher" || userInfo.role == "Student")
+      throw new UnauthorizedException("You are not authorized")
+    const user = await this.authService.register(registerDto);
+    return response.status(201).send(user);
   }
 
 }
