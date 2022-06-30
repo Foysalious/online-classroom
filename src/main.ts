01e8cfbd9d51b84b66c8ab6ae30502a4c038d7f5
foysalious <foysalrahman112@gmail.com>
2022-06-30 18:49:43 +0600

    get port

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+require('dotenv').config()
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,11 +10,10 @@ async function bootstrap() {
     .setDescription('Online Classroom')
     .setVersion('1.0')
     .addBearerAuth()
-    .build(); 
- 
+    .build();
+
   const document = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('api', app, document);
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
- 
